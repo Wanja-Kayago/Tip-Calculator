9f630c2c309b2de37c3faeba4efc5df30b0688d6
esther-njoroge <nyokabinjorogeesther@gmail.com>
2024-05-19 18:52:13 +0300

    tip and total

--- script.js
+++ script.js
@@ -15,11 +15,29 @@ calculateBtn.addEventListener('click', function() {
       tipPercentage = 0.18;
     } else {
       tipPercentage = 0.2;
-    } })
+    } 
 
     const tipAmount = billTotal * tipPercentage / noOfPeople;
   const totalBill = billTotal/noOfPeople + tipAmount;
   const tipPerPerson = tipAmount / noOfPeople;
+
   totalTipOutput.textContent = `Ksh ${tipAmount.toFixed(2)}`;
   totalBillOutput.textContent = `Ksh ${totalBill.toFixed(2)}`;
   tipPerPersonOutput.textContent = `Ksh ${tipPerPerson.toFixed(2)}`;
+
+  const buttons = [btn15, btn18, btn20];
+  for (const button of buttons) {
+    if (button.classList.contains('active')) {
+      button.style.backgroundColor = 'yellow';
+      button.textContent = 'Selected: ' + button.textContent.slice(4);
+    } else {
+      button.innerHTML.style.backgroundColor = 'blue';
+      button.textContent = button.textContent.slice(0, 3);
+    }
+  }
+  if (noOfPeople === 1) {
+    tipPerPersonOutput.parentElement.style.display = 'none';
+  } else {
+    tipPerPersonOutput.parentElement.style.display = 'block';
+  }
+});
