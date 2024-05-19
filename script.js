b7fd6109030cde855ecb77eb8fa4edef9afcd1b9
stephanie-niva <stephanienivaa@example.com>
2024-05-19 18:19:17 +0300

    percentage changes

--- script.js
+++ script.js
@@ -3,4 +3,16 @@ const noOfPeopleInput = document.getElementById('noOfPeople');
 const calculateBtn = document.getElementById('calculate');
 const totalTipOutput = document.getElementById('totalTip');
 const totalBillOutput = document.getElementById('totalBill');
-const tipPerPersonOutput = document.getElementById('tipPerPerson'); 
+const tipPerPersonOutput = document.getElementById('tipPerPerson');
+
+calculateBtn.addEventListener('click', function() {
+    const billTotal = parseFloat(billTotalInput.value);
+    const noOfPeople = parseFloat(noOfPeopleInput.value);
+    let tipPercentage;
+    if (btn15.classList.contains('active')) {
+      tipPercentage = 0.15;
+    } else if (btn18.classList.contains('active')) {
+      tipPercentage = 0.18;
+    } else {
+      tipPercentage = 0.2;
+    } })
