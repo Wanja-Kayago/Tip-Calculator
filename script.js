8021dc0c6d400e4e0155593e81d90bb99f613019
Wanja-Kayago <kayagonaomiwanja@gmail.com>
2024-05-19 18:00:08 +0300

    add boiler-plate

---
+++ script.js
@@ -0,0 +1,6 @@
+const billTotalInput = document.getElementById('billTotal');
+const noOfPeopleInput = document.getElementById('noOfPeople');
+const calculateBtn = document.getElementById('calculate');
+const totalTipOutput = document.getElementById('totalTip');
+const totalBillOutput = document.getElementById('totalBill');
+const tipPerPersonOutput = document.getElementById('tipPerPerson'); 
